2f8ad01a388ef337c04a0829d40c138d56840934
Lacamoura48 <anasfog@outlook.com>
2022-07-31 22:38:28 +0100

    work in progress section haha

--- src/components/main/divBotLeft/DivBotLeft.vue
+++ src/components/main/divBotLeft/DivBotLeft.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="divBotLeft rounded-[50px] bg-white">
-    hello
+  <div class="divBotLeft rounded-[50px] bg-white p-10 w-full flex flex-col">
+    <h1 class="font-extrabold text-[#FF6E6A] text-[36px]">More...</h1>
+    <div class="flex h-full justify-around items-center px-4">
+      <div class="border-r border-1 h-1/2 flex items-center pr-10">
+        <font-awesome-icon icon="fa-solid fa-gear" class="text-[80px] text-[#dddddd] gearone animate-spin"/>
+        <font-awesome-icon icon="fa-solid fa-gear" class="text-[60px] text-[#c5c5c5] geartwo"/>
+      </div>
+      
+      
+        
+        <h2 class="text-[25px] text-[#B9B9B9]">Work is still in progress...</h2>
+      
+    </div>
 
   </div>
 </template>
@@ -12,5 +23,19 @@ export default {
 </script>
 
 <style>
-
+ h1 {
+        font-family: 'Dosis';
+        
+    }
+    .gearone{
+      animation: spin 6s linear infinite !important;
+      animation-delay: 0.5s !important;
+    }
+    .geartwo{
+      animation: spin 6s reverse linear infinite !important;
+    }
+    h2 {
+       
+    }
+    
 </style>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue'
 import router from './router'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHouse, faCircleInfo } from '@fortawesome/free-solid-svg-icons'
+import { faHouse, faCircleInfo, faGear } from '@fortawesome/free-solid-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 
-library.add(faHouse, faCircleInfo, faGithub)
+library.add(faHouse, faCircleInfo, faGithub, faGear)
 
 
 
