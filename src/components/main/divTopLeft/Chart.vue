99e6887de1a898827b114838a35c76ba322dc362
Lacamoura48 <anasfog@outlook.com>
2022-08-01 16:26:44 +0100

    I ANIMATED THE CHART !!

--- src/components/main/divTopLeft/Chart.vue
+++ src/components/main/divTopLeft/Chart.vue
@@ -1,11 +1,17 @@
 <template>
-<div class="relative w-[240px] h-[240px]">
-  <div class="w-[225px] h-[225px] rounded-full border-8 absolute top-[7px] left-[7px]"></div>
-  <div v-bind:style="{transition: 'background 0.5s',background : editOption }" id="chart" class="w-[240px] h-[240px] rounded-full absolute"></div>
-  <div  class="w-[195px] h-[195px] rounded-full absolute bg-white top-[23px] left-[22px] flex justify-center items-center">
+<div class="relative w-[280px] h-[280px]">
+ 
+  <!-- <div v-bind:style="{transition: 'background 0.5s',background : editOption }" id="chart" class="w-[240px] h-[240px] rounded-full absolute"></div> -->
+  <svg focusable="false" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" tabindex="-1" width="280"  viewBox="0 0 100 100">
+         <circle cx="50" cy="50" r="40" stroke="#F2F2F2" stroke-width="3" fill="none" />
+        <circle id="bar" cx="50" cy="50" r="40" :stroke="percentage < 50 ? '#FFB3B3' : percentage < 100 ? '#FFDD78' : '#99FFBC'" stroke-width="6" :stroke-dasharray="`${percentage} 100`" fill="none" pathLength="100" transform="rotate(90 50 50)"/>
+       
+        
+      </svg>
+  <div  class="w-[160px] h-[160px] rounded-full absolute top-[59px] left-[60px] flex justify-center items-center">
     <div>
-        <span class=" flex justify-center items-center text-[34px]">{{percentage}}%</span>
-        <p class="text-[21px]">tasks completed</p>
+        <span class=" flex justify-center items-center text-[32px]">{{percentage}}%</span>
+        <p class="text-[17px]">tasks completed</p>
     </div>
   </div>
   </div>
@@ -13,19 +19,11 @@
 
 <script>
 export default {
-    data(){
-        return{
-            editOption : `conic-gradient(#FFDD78 0% 0%,transparent 0% 100%)`
-        }
-    },
+   
     props : {
         percentage : Number,
     },
-    watch : {
-        percentage:function(newv){
-            this.editOption = `conic-gradient(#FFDD78 0% ${newv}%,transparent ${newv}% 100%)`
-        }
-    }
+    
 }
 </script>
 
@@ -35,6 +33,9 @@ span, p {
     font-family: 'Nunito';
     font-weight: 300;
     color: #C6C6C6;
+}
+#bar {
+    transition: all cubic-bezier(0.215, 0.610, 0.355, 1) 0.6s;
 }
 
 </style>
